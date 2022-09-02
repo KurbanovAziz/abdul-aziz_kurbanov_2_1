136b1065a482e2ebcdf3f8059e1cf83a1d05c5e7
KurbanovAziz <ripplemobile29@gmail.com>
2022-09-02 20:28:03 +0600

    returnChange

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,41 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        ageTemperature(21, -10);
-        ageTemperature(19, 0);
-        ageTemperature(50, 25);
-        ageTemperature(65, 0);
-        ageTemperature(70, -100);
-        ageTemperature(generateRandomAge(), -10);
+        String resultOne = ageTemperature(21, -10);
+        String resultTwo = ageTemperature(19, 0);
+        String resultThree = ageTemperature(50, 25);
+        String resultFour = ageTemperature(65, 0);
+        String resultHome = ageTemperature(70, -100);
+        String resultRandom = ageTemperature(generateRandomAge(), -10);
+        System.out.println(resultOne);
+        System.out.println(resultTwo);
+        System.out.println(resultThree);
+        System.out.println(resultFour);
+        System.out.println(resultHome);
+        System.out.println(resultRandom);
 
     }
 
     public static String ageTemperature(int age, int temperature) {
 
         if ((age >= 20 || age == 45) && (temperature >= -20 || temperature == 30)) {
-            goToWalk();
+            return goToWalk();
         } else if (age < 20 && (temperature >= 0 || temperature == 28)) {
-            goToWalk();
+            return goToWalk();
         } else if (age > 45 && (temperature >= -10 || temperature == 25)) {
-            goToWalk();
+            return goToWalk();
         } else {
-            System.out.println("Оставайся дома");
+            return goToHome();
         }
-
-        return "";
-
     }
 
 
-    public static void goToWalk() {
-        System.out.println("Можно идти гулять");
+    public static String goToWalk() {
+        return "Можно идти гулять";
 
+    }
+    public static String goToHome() {
+        return "Оставайся дома";
     }
 
     public static int generateRandomAge() {
